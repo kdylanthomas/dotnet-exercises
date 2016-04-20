ddf13f97438cd2c708460c15701ea9bd6af7e922
Dylan Thomas <kdylanthomas@gmail.com>
2016-04-20 17:12:09 -0500

    passing unit tests for integers and fibonaccis

---
+++ MathMagician/MathMagician/NumberPatterns/Fibonacci.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMagician.NumberPatterns
+{
+   public class Fibonacci : Integer
+    {
+        public List<int> allFibonaccis { get; set; }
+
+        public Fibonacci()
+        {
+            initial = 1;
+            allFibonaccis = new List<int>();
+        }
+
+        public List<int> createFibonacciList()
+        {
+            bool keepGoing = true;
+            int i = initial;
+            allFibonaccis.Add(initial);
+            allFibonaccis.Add(initial);
+
+            while (keepGoing == true)
+            {
+                if (i > 20) // temporary way to manage limit until I implement readkey
+                {
+                    keepGoing = false;
+                    break;
+                }
+                var currFibArr = allFibonaccis.ToArray();
+                int currLength = currFibArr.Length;
+                int firstNumber = currFibArr[currLength - 2];
+                int secondNumber = currFibArr[currLength - 1];
+                i = firstNumber + secondNumber;
+                allFibonaccis.Add(i);
+            }
+            return allFibonaccis;
+        }
+
+    }
+}
---
+++ MathMagician/MathMagician/NumberPatterns/Integer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMagician.NumberPatterns
+{
+    public class Integer
+    {
+        public int initial { get; set; } // starting value for num list
+        public int increment { get; set; } // increment by how many? 
+        public List<int> allIntegers { get; set; }
+
+        public Integer()
+        {
+            initial = 1;
+            increment = 1;
+            allIntegers = new List<int>(); // initialize list w/ first number
+        }
+
+        // method needed:
+        // if current + increment is an integer, add that number to the list
+
+        public List<int> createIntegerList()
+        {
+            int i = initial;
+            bool keepGoing = true;
+
+            while (keepGoing == true)
+            {
+                if (i > 10) // temporary in place of ReadKey
+                {
+                    keepGoing = false;
+                    break;
+                }
+                Console.WriteLine("allIntegers is {0}", allIntegers);
+                Console.WriteLine("i is {0}", i);
+                allIntegers.Add(i);
+                i += increment;
+            }
+            return allIntegers;
+        }
+
+    }
+}
