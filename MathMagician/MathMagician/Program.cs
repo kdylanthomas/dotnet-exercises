fad171102d4e70a733cc330a0ccbcaff4450a6d9
Dylan Thomas <kdylanthomas@gmail.com>
2016-04-21 11:23:10 -0500

    got prime numbers unit test to pass

--- MathMagician/MathMagician/Program.cs
+++ MathMagician/MathMagician/Program.cs
@@ -11,32 +11,32 @@ namespace MathMagician
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I am the Math Magician. What would you like me to do?");
-            Console.WriteLine("Choose integers, primes, or fibonaccis.");
-            string userInput = Console.ReadLine();
+            //Console.WriteLine("I am the Math Magician. What would you like me to do?");
+            //Console.WriteLine("Choose integers, primes, or fibonaccis.");
+            //string userInput = Console.ReadLine();
 
-            switch (userInput)
-            {
-                case "integers":
-                    break;
-                case "primes":
-                    break;
-                case "fibonaccis":
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
+            //switch (userInput)
+            //{
+            //    case "integers":
+            //        break;
+            //    case "primes":
+            //        break;
+            //    case "fibonaccis":
+            //        break;
+            //    default:
+            //        throw new ArgumentOutOfRangeException();
+            //}
         
-            Console.WriteLine("Press any key to exit.");
-            var quit = Console.ReadKey();
-            if (quit != null) Environment.Exit(0);
+            //Console.WriteLine("Press any key to exit.");
+            //var quit = Console.ReadKey();
+            //if (quit != null) Environment.Exit(0);
 
             //Integer ints = new Integer();
             //ints.createIntegerList();
             //Fibonacci fibs = new Fibonacci();
             //fibs.createFibonacciList();
-            //Prime primes = new Prime();
-            //primes.createPrimesList();
+            Prime primes = new Prime();
+            primes.createPrimesList();
             // Main should take user input and determine
             // which method to run based on input
         }
