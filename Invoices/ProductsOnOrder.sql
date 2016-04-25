574d7d250765ed69312d8295b38ff1f0756d3238
Dylan Thomas <kdylanthomas@gmail.com>
2016-04-25 16:01:24 -0500

    added queries

---
+++ Invoices/ProductsOnOrder.sql
@@ -0,0 +1,14 @@
+﻿-- goal: get the names of products that are on an order
+SELECT
+ co.DateCreated,
+ co.OrderNumber,
+ p.Name,
+ pt.Name as ProductTypeName,
+ c.FirstName + ' ' + c.LastName AS CustomerName,
+ po.Name as Payment
+FROM CustomerOrder co
+INNER JOIN OrderProducts op ON op.IdCustomerOrder = co.IdCustomerOrder 
+INNER JOIN Product p ON p.IdProduct = op.IdProduct
+INNER JOIN ProductType pt ON pt.IdProductType = p.IdProductType
+INNER JOIN Customer c ON c.IdCustomer = co.IdCustomer 
+INNER JOIN PaymentOption po ON po.IdPaymentOption = co.IdPaymentOption
