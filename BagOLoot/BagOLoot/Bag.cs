c8f9a1482fa996ece3d85542d6679fa5d270ddb6
dylan thomas <dylan thomas>
2016-04-26 11:46:04 -0500

    wrote passing unit tests for Bag and Child classes, started functionality in Main

---
+++ BagOLoot/BagOLoot/Bag.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BagOLoot
+{
+    public class Bag
+    {
+        // needs Add method
+        public void Add(Child child, string toy)
+        {
+            child.Toys.Add(toy);
+        }
+
+        // needs Remove method
+        public void Remove(Child child, string toy)
+        {
+            if (child.Toys.Contains(toy)) child.Toys.Remove(toy);
+        }
+
+        // needs List of toys?
+        // or, list of children that points to their list of toys?
+        public List<Child> ChildrenWithToys { get; set; }
+
+        public Bag()
+        {
+            ChildrenWithToys = new List<Child>();
+        }
+    }
+}
---
+++ BagOLoot/BagOLoot/Child.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BagOLoot
+{
+    public class Child
+    {
+        // needs name
+        public string Name { get; set; }
+        // needs list of toys
+        public List<string> Toys { get; set; }
+        // bool toysDelivered
+        public bool ToysDelivered { get; set; }
+
+        public Child(string name)
+        {
+            Name = name;
+            Toys = new List<string>();
+            ToysDelivered = false;
+        }
+    }
+}
