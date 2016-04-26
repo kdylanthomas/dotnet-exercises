41006a8abd851d181d42cf8039b9b979268f88ef
dylan thomas <dylan thomas>
2016-04-26 11:47:48 -0500

    wrote passing unit tests for Bag and Child classes, started functionality in Main

---
+++ BagOLoot/BagOLootTests/BagTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BagOLoot;
+using System.Linq;
+
+namespace BagOLootTests
+{
+    [TestClass]
+    public class BagTests
+    {
+        [TestMethod]
+        public void CanCreateInstanceOfBag()
+        {
+            Bag bag = new Bag();
+            Assert.IsNotNull(bag);
+        }
+
+        [TestMethod]
+        public void BagContainsListOfChildren()
+        {
+            Bag bag = new Bag();
+            Assert.IsInstanceOfType(bag.ChildrenWithToys, typeof(List<Child>));
+        }
+
+        [TestMethod]
+        public void CanAddToysToBag()
+        {
+            Bag bag = new Bag();
+            Child bobby = new Child("Bobby");
+            bag.Add(bobby, "model airplane");
+            Assert.IsTrue(bobby.Toys.First() == "model airplane");
+        }
+
+        [TestMethod]
+        public void CanRemoveToysFromBag()
+        {
+            Bag bag = new Bag();
+            Child bobby = new Child("Bobby");
+            bobby.Toys = new List<string>{"model airplane", "action figure"};
+            bag.Remove(bobby, "action figure");
+            Assert.IsFalse(bobby.Toys.Contains("action figure"));
+        }
+
+        [TestMethod]
+        public void RemovingToysFromBagDoesNotAffectToysStillInBag()
+        {
+            Bag bag = new Bag();
+            Child bobby = new Child("Bobby");
+            bobby.Toys = new List<string> { "model airplane", "action figure" };
+            bag.Remove(bobby, "action figure");
+            Assert.IsTrue(bobby.Toys.Contains("model airplane"));
+        }
+    }
+}
---
+++ BagOLoot/BagOLootTests/ChildTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BagOLoot;
+
+namespace BagOLootTests
+{
+    [TestClass]
+    public class ChildTests
+    {
+        [TestMethod]
+        public void CanCreateInstanceOfChild()
+        {
+            Child joey = new Child("Joey");
+            Assert.IsNotNull(joey);
+        }
+
+        [TestMethod]
+        public void InstancesOfChildHaveListOfToys()
+        {
+            Child joey = new Child("Joey");
+            Assert.IsInstanceOfType(joey.Toys, typeof(List<string>));
+        }
+
+        [TestMethod]
+        public void ToysAreInitiallyNotDelivered()
+        {
+            Child joey = new Child("Joey");
+            Assert.AreEqual(joey.ToysDelivered, false);
+        }
+
+        [TestMethod]
+        public void ToysCanBeDelivered()
+        {
+            Child joey = new Child("Joey");
+            joey.ToysDelivered = true;
+            Assert.AreEqual(joey.ToysDelivered, true);
+        }
+    }
+}
