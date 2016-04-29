55bc6a89b816650d5bb764cfb87df7716c15338c
dylan thomas <dylan thomas>
2016-04-28 22:41:08 -0500

    added sample data to tables, created Customer class w/ parse name method, initial terminal prompts

---
+++ Bangazon/Bangazon/Terminal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bangazon
+{
+    public class Terminal
+    {
+
+        public string userInput
+        {
+            get { return userInput; }
+            set
+            {
+                if (value != "") userInput = value;
+            }
+        }
+        public string LoadMessage { get; set; }
+        public Terminal()
+        {
+            LoadMessage = @"*********************************************************
+* * Welcome to Bangazon! Command Line Ordering System * *
+*********************************************************
+1.Create an account
+2.Create a payment option
+3.Order a product
+4.Complete an order
+5.See product popularity
+6.Leave Bangazon!
+> ";
+
+        }
+    }
+}
