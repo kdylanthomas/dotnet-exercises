e4a0c74e2e807a15da12d165f7f7f6ec88981deb
dylan thomas <dylan thomas>
2016-05-02 15:36:22 -0500

    restructured app, defined connection string for sql inserts, customers can be created

--- Bangazon/Bangazon/Terminal.cs
+++ Bangazon/Bangazon/Terminal.cs
@@ -8,19 +8,12 @@ namespace Bangazon
 {
     public class Terminal
     {
+        SqlData sqlData = new SqlData();
 
-        public string userInput
+        public void ShowMenu()
         {
-            get { return userInput; }
-            set
-            {
-                if (value != "") userInput = value;
-            }
-        }
-        public string LoadMessage { get; set; }
-        public Terminal()
-        {
-            LoadMessage = @"*********************************************************
+            Console.Write(
+@"*********************************************************
 * * Welcome to Bangazon! Command Line Ordering System * *
 *********************************************************
 1.Create an account
@@ -29,8 +22,39 @@ namespace Bangazon
 4.Complete an order
 5.See product popularity
 6.Leave Bangazon!
-> ";
+> ");
+            string userSelection = Console.ReadLine();
 
+            switch (userSelection)
+            {
+                case "1":
+                    CreateNewCustomer();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void CreateNewCustomer()
+        {
+            Customer cust = new Customer();
+            cust.IdCustomer = 5;
+            Console.WriteLine("Enter first name \n>");
+            cust.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter last name \n>");
+            cust.LastName = Console.ReadLine();
+            Console.WriteLine("Enter street address \n>");
+            cust.StreetAddress = Console.ReadLine();
+            Console.WriteLine("Enter city \n>");
+            cust.City = Console.ReadLine();
+            Console.WriteLine("Enter state \n>");
+            cust.State = Console.ReadLine();
+            Console.WriteLine("Enter postal code \n>");
+            cust.PostalCode = Console.ReadLine();
+            Console.WriteLine("Enter phone number \n>");
+            cust.PhoneNumber = Console.ReadLine();
+
+            sqlData.CreateCustomer(cust);
         }
     }
 }
