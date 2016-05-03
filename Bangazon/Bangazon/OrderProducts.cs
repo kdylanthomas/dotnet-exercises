7d957391b7bb937e484d09b4e099e668bb4d3de5
dylan thomas <dylan thomas>
2016-05-03 15:24:24 -0500

    finished basic requirements for exercise

--- Bangazon/Bangazon/OrderProducts.cs
+++ Bangazon/Bangazon/OrderProducts.cs
@@ -11,7 +11,9 @@ namespace Bangazon
         public int IdOrderProducts { get; set; }
         public int IdProduct { get; set; }
         public int IdCustomerOrder { get; set; }
+        public int IdCustomer { get; set; }
         public int Count { get; set; }
+        public int CustomerCount { get; set; }
         public string Price { get; set; }
         public string Name { get; set; }
     }
