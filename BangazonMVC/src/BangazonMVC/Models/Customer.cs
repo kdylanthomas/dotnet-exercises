ed8a23c002301a65553164b9f298c4252d23c155
dylan thomas <dylan thomas>
2016-05-13 11:26:08 -0500

    created models and did initial migration

---
+++ BangazonMVC/src/BangazonMVC/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonMVC.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string StateProvince { get; set; }
+        public string PostalCode { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ BangazonMVC/src/BangazonMVC/Models/PaymentOption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonMVC.Models
+{
+    public class PaymentOption
+    {
+        public int Id { get; set; }
+        public int IdCustomer { get; set; }
+        public string Name { get; set; }
+        public string AccountNumber { get; set; }
+    }
+}
