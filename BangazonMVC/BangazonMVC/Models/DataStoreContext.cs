fdc0b4cb60e98d9256765a056bb4721c858c24fd
dylan thomas <kdylanthomas@gmail.com>
2016-05-17 16:00:24 -0500

    app manages CRUD operations on customers

--- BangazonMVC/BangazonMVC/Models/DataStoreContext.cs
+++ BangazonMVC/BangazonMVC/Models/DataStoreContext.cs
@@ -9,11 +9,11 @@ namespace BangazonMVC.Models
     public class DataStoreContext : DbContext
     {
         public DbSet<Customer> Customer { get; set; }
-        public DbSet<CustomerOrder> CustomerOrder { get; set; }
-        public DbSet<OrderProduct> OrderProduct { get; set; }
-        public DbSet<PaymentOption> PaymentOption { get; set; }
-        public DbSet<Product> Product { get; set; }
-        public DbSet<ProductType> ProductType { get; set; }
+        //public DbSet<CustomerOrder> CustomerOrder { get; set; }
+        //public DbSet<OrderProduct> OrderProduct { get; set; }
+        //public DbSet<PaymentOption> PaymentOption { get; set; }
+        //public DbSet<Product> Product { get; set; }
+        //public DbSet<ProductType> ProductType { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
@@ -21,25 +21,25 @@ namespace BangazonMVC.Models
                 .ToTable("Customer")
                 .HasKey(c => c.IdCustomer);
  
-            modelBuilder.Entity<CustomerOrder>()
-                .ToTable("CustomerOrder")
-                .HasKey(o => o.IdCustomerOrder);
+            //modelBuilder.Entity<CustomerOrder>()
+            //    .ToTable("CustomerOrder")
+            //    .HasKey(o => o.IdCustomerOrder);
 
-            modelBuilder.Entity<OrderProduct>()
-                .ToTable("OrderProduct")
-                .HasKey(op => op.IdOrderProducts);
+            //modelBuilder.Entity<OrderProduct>()
+            //    .ToTable("OrderProduct")
+            //    .HasKey(op => op.IdOrderProducts);
 
-            modelBuilder.Entity<PaymentOption>()
-                .ToTable("Product")
-                .HasKey(po => po.IdPaymentOption);
+            //modelBuilder.Entity<PaymentOption>()
+            //    .ToTable("PaymentOption")
+            //    .HasKey(po => po.IdPaymentOption);
 
-            modelBuilder.Entity<Product>()
-                .ToTable("Product")
-                .HasKey(p => p.IdProduct);
+            //modelBuilder.Entity<Product>()
+            //    .ToTable("Product")
+            //    .HasKey(p => p.IdProduct);
 
-            modelBuilder.Entity<ProductType>()
-                .ToTable("ProductType")
-                .HasKey(pt => pt.IdProductType);
+            //modelBuilder.Entity<ProductType>()
+            //    .ToTable("ProductType")
+            //    .HasKey(pt => pt.IdProductType);
         }
 }
 }
